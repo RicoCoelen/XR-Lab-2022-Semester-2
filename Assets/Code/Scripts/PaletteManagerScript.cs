65a2060e226d3720275eb917d9971705df408015
DESKTOP-VRStudent <rico.coelen@gmail.com>
2022-11-15 13:01:22 +0100

    added some code for drawing and cleaned it up

--- Assets/Code/Scripts/PaletteManagerScript.cs
+++ Assets/Code/Scripts/PaletteManagerScript.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 using Valve.VR;
 
 public class PaletteManagerScript : MonoBehaviour
 {
+    #region Properties
+
     [Header("Prefabs")]
     public GameObject brushPrefab;
 
@@ -14,9 +17,21 @@ public class PaletteManagerScript : MonoBehaviour
     [Header("GameObjects")]
     public GameObject brushGO;
 
+    [Header("Input Action Variables")]
+    public SteamVR_Action_Boolean DrawingAction; // reference to actionfile 
+    public Hand hand; // the reference to the VR hand
+    public GameObject prefabToPlant; // plant to spawn
+
     [Header("Painting Variables")]
     public Line[] lines;
 
+    #endregion
+
+    #region Classes
+
+    /// <summary>
+    ///   Class to hold the line variables
+    /// </summary>
     public class Line
     {
         public int order;
@@ -24,25 +39,81 @@ public class PaletteManagerScript : MonoBehaviour
         public Color color;
     }
 
-    // Start is called before the first frame update
+    #endregion
+
+    #region Constructor
+    /// <summary>
+    /// awake function to ready up for game start
+    /// </summary>
     void Awake()
     {
         brushGO = Instantiate(brushPrefab, brushSpot.transform);
     }
 
-    public void FixedUpdate()
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// enable event handler, also on hand
+    /// </summary>
+    private void OnEnable()
     {
-        //if (SteamVR_Input._default.)
-        //{
-        //    brushGO.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //}
+        if (hand == null)
+            hand = this.GetComponent<Hand>();
+
+        if (DrawingAction == null)
+        {
+            Debug.LogError("<b>[SteamVR Interaction]</b> No plant action assigned", this);
+            return;
+        }
+
+        DrawingAction.AddOnChangeListener(OnDrawingActionChange, hand.handType);
     }
 
+    /// <summary>
+    /// disable event handler 
+    /// </summary>
+    private void OnDisable()
+    {
+        if (DrawingAction != null)
+            DrawingAction.RemoveOnChangeListener(OnDrawingActionChange, hand.handType);
+    }
 
+    /// <summary>
+    /// change value if event listener changes
+    /// </summary>
+    /// <param name="actionIn"> </param>
+    /// <param name="inputSource">  </param>
+    /// <param name="newValue"> the new returned variable </param>
+    private void OnDrawingActionChange(SteamVR_Action_Boolean actionIn, SteamVR_Input_Sources inputSource, bool newValue)
+    {
+        if (newValue)
+        {
+           // Plant();
+        }
+    }
+
+    /// <summary>
+    /// fixedupdate to update values at fixed rate
+    /// </summary>
+    public void FixedUpdate()
+    {
+        if (DrawingAction.active)
+        {
+            brushGO.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    /// <summary>
+    /// return or rest tools to their original location
+    /// </summary>
+    /// <param name="objectTouch"> gives the reference to the game object that is touched </param>
     public void ReturnTools(GameObject objectTouch)
     {
         brushGO.transform.parent = transform;
         brushGO.transform.position = brushSpot.transform.position;
         brushGO.transform.rotation = brushSpot.transform.rotation;
     }
+    #endregion
 }
