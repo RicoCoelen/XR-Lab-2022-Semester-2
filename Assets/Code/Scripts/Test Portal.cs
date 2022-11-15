581105b92e67e411c5e5a41871708a767720a728
Anneys <annebleys@gmail.com>
2022-11-15 12:13:04 +0100

    Kloten met portaal

---
+++ Assets/Code/Scripts/Test Portal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TestPortal : MonoBehaviour
+{
+    public GameObject _point1;
+    public GameObject _point2;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<Renderer>().gameObject.tag == "Brush" && )
+        {
+            if 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
