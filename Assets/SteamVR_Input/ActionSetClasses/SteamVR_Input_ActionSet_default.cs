183b2cad3727856934b019e8609301f1dc44d20a
DESKTOP-VRStudent <rico.coelen@gmail.com>
2022-11-15 11:51:27 +0100

    Added SteamVR input file for drawing

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -105,6 +105,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean DrawingInput
+        {
+            get
+            {
+                return SteamVR_Actions.default_DrawingInput;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
