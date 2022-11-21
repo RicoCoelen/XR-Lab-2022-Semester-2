3fb3ff64c15b447b190daf9f1df26395417cfc66
DESKTOP-VRStudent <rico.coelen@gmail.com>
2022-11-21 13:00:44 +0100

    made drawing work, still has artifacts i need to tfix

--- Assets/Code/Scripts/PaletteManagerScript.cs
+++ Assets/Code/Scripts/PaletteManagerScript.cs
@@ -20,13 +20,13 @@ public class PaletteManagerScript : MonoBehaviour
     [Header("Input Action Variables")]
     // public SteamVR_Action_Single TriggerAction; // reference to actionfile 
     public SteamVR_Action_Boolean GripAction; // reference to actionfile 
-    public SteamVR_Action_Boolean DrawTriggerAction; // reference to actionfile 
+    public SteamVR_Action_Single DrawTriggerAction; // reference to actionfile 
     public SteamVR_Input_Sources AllDevices = SteamVR_Input_Sources.Any;
 
     [Header("Painting Variables")]
     public List<Line> lines = new List<Line>();
     public bool grip = false;
-    public bool trigger = false;
+    public float trigger = 0f;
     public Hand LeftHand;
     public Hand RightHand;
     public Material currentMaterial;
@@ -51,14 +51,17 @@ public class PaletteManagerScript : MonoBehaviour
 
         public Line(int order, Vector3 pos, Material mat)
         {
-            gameObject = new GameObject();
 
-            gameObject.transform.position = pos;
-            points.Add(pos);
+            Debug.Log(pos);
+            gameObject = new GameObject();
+            gameObject.transform.position = Vector3.zero;
 
             lr = gameObject.AddComponent<LineRenderer>();
             lr.material = mat;
             lr.widthMultiplier = 0.1f;
+
+            points.Add(pos);
+            lr.SetPosition(0, pos);
 
             rb = gameObject.AddComponent<Rigidbody>();
             rb.isKinematic = true;
@@ -69,9 +72,10 @@ public class PaletteManagerScript : MonoBehaviour
 
         public void AddPoint(Vector3 pos)
         {
+            Debug.Log(pos);
             points.Add(pos);
+            lr.positionCount = points.Count;
             lr.SetPositions(points.ToArray());
-            Debug.Log(points.ToArray().ToString());
         }
     }
 
@@ -96,54 +100,7 @@ public class PaletteManagerScript : MonoBehaviour
 
     #region EventListener
 
-    /// <summary>
-    /// enable input event handler on hand
-    /// </summary>
-    private void OnEnable()
-    {
-        // add event listeners
-        GripAction.AddOnChangeListener(OnGripActionChange, AllDevices);
-        DrawTriggerAction.AddOnChangeListener(OnDrawTriggerActionChange, AllDevices);
-    }
-
-    /// <summary>
-    /// disable event handler 
-    /// </summary>
-    private void OnDisable()
-    {
-        if (GripAction != null)
-        {
-            GripAction.RemoveOnChangeListener(OnGripActionChange, AllDevices);
-        }
-
-        if (DrawTriggerAction != null)
-        { 
-            DrawTriggerAction.RemoveOnChangeListener(OnDrawTriggerActionChange, AllDevices);
-        }
-
-    }
-
-    /// <summary>
-    /// change value if Grip listener changes
-    /// </summary>
-    /// <param name="action"> </param>
-    /// <param name="inputSource">  </param>
-    /// <param name="newValue"> the new returned variable </param>
-    private void OnGripActionChange(SteamVR_Action_Boolean action, SteamVR_Input_Sources inputSource, bool value)
-    {
-        this.grip = value;
-    }
-
-    /// <summary>
-    /// change value if trigger listener changes
-    /// </summary>
-    /// <param name="action"> </param>
-    /// <param name="inputSource">  </param>
-    /// <param name="newValue"> the new returned variable </param>
-    private void OnDrawTriggerActionChange(SteamVR_Action_Boolean action, SteamVR_Input_Sources inputSource, bool value)
-    {
-        this.trigger = value;
-    }
+    // TODO:
 
     #endregion
 
@@ -164,12 +121,18 @@ public class PaletteManagerScript : MonoBehaviour
         //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 
+    private void Update()
+    {
+        trigger = DrawTriggerAction.GetAxis(AllDevices);
+        grip = GripAction.GetState(AllDevices);
+    }
+
     /// <summary>
     /// fixedupdate to update values at fixed rate
     /// </summary>
     public void FixedUpdate()
     {
-        if (trigger)
+        if (trigger > 0)
         {
             if (LeftHand.currentAttachedObject == brushGO.gameObject)
             {
