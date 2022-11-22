e4bb6230c48510a213ce62f7c70c44b9b61cb3cc
Anneys <annebleys@gmail.com>
2022-11-22 10:51:46 +0100

    Klaar voor de teleport test 1

--- Assets/Code/Scripts/TestTeleportal.cs
+++ Assets/Code/Scripts/TestTeleportal.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TestTeleportal : MonoBehaviour
 {
     public GameObject _player;
+
+    //if the player collides with the portal, the player is teleported to a certain location in the scene. 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            _player.transform.position = new Vector3(3, 0, 0);
+            _player.transform.position = new Vector3(35, 15, 0);
             Debug.Log("hi");
         }
     }
