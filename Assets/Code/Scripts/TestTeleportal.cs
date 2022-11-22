7917f831d44780678aecff862ea2c53ce8448cce
Anneys <annebleys@gmail.com>
2022-11-22 13:37:13 +0100

    Poging 1 canvast setactive at teleport

--- Assets/Code/Scripts/TestTeleportal.cs
+++ Assets/Code/Scripts/TestTeleportal.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class TestTeleportal : MonoBehaviour
 {
     public GameObject _player;
+    public GameObject _teleportview;
 
     //if the player collides with the portal, the player is teleported to a certain location in the scene. 
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
+            _teleportview.SetActive(true);
             _player.transform.position = new Vector3(30, 15, 0);
             Debug.Log("hi");
         }
