5a1816ec9d8bedd261d0881b9384dc5a487da20c
DESKTOP-VRStudent <rico.coelen@gmail.com>
2022-11-28 14:46:27 +0100

    Kan nu teleporteren tussen scenes

--- Assets/Code/Scripts/Teleportal.cs
+++ Assets/Code/Scripts/Teleportal.cs
@@ -1,16 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using Scene = UnityEngine.SceneManagement.Scene;
 
 public class Teleportal : MonoBehaviour
 {
     public GameObject _player;
     public GameObject _teleportview1;
     public GameObject _teleportview2;
-    public string _dreamscene;
+    [SerializeField] private string newScene;
 
     //if the player collides with the portal, the player is teleported to a certain location in the scene. 
     void OnTriggerEnter(Collider collider)
@@ -22,20 +20,13 @@ public class Teleportal : MonoBehaviour
     }
     IEnumerator Timertje()
     {
-        _teleportview1.SetActive(true);
-        yield return new WaitForSeconds(2);
-        Scene currentScene = SceneManager.GetActiveScene();
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(_dreamscene, LoadSceneMode.Additive);
-        while (!asyncLoad.isDone)
-        {
-            yield return null;
-        }
-        SceneManager.MoveGameObjectToScene(_player, SceneManager.GetSceneByName(_dreamscene));
-        SceneManager.UnloadSceneAsync(currentScene);
-        yield return new WaitForSeconds(2);
-        _teleportview2.SetActive(true);
-        _teleportview1.SetActive(false);
-        yield return new WaitForSeconds(4);
-        _teleportview2.SetActive(false);
+            _teleportview1.SetActive(true);
+            yield return new WaitForSeconds(2);
+            SceneManager.LoadScene("DreamWorld");
+            yield return new WaitForSeconds(2);
+            _teleportview2.SetActive(true);
+            _teleportview1.SetActive(false);
+            yield return new WaitForSeconds(4);
+            _teleportview2.SetActive(false);
     }
 }
