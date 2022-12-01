e58bb4c508456d301a4f38354a7f6244414cf77b
dell-optiplex\Casper <casperarends1910@gmail.com>
2022-12-01 11:52:05 +0100

    extra comments and minimap texture

--- Assets/Code/Scripts/Minimap.cs
+++ Assets/Code/Scripts/Minimap.cs
@@ -6,14 +6,12 @@ public class Minimap : MonoBehaviour
 {
     public Transform player;
 
+    //If the this object is enabled follow the players position and y rotation.
     public void LateUpdate()
     {
         Vector3 newPosition = player.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
-
         transform.rotation = Quaternion.LookRotation(Vector3.down);
-        //transform.rotation = Quaternion.Euler(Vector3.down);
-
     }
 }
