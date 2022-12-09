864b64c51e1c0fdc33f1b97c91be1272ba2ce5ac
dell-optiplex\Casper <casperarends1910@gmail.com>
2022-12-09 10:34:41 +0100

    minimap added scriptable objects and minimap telephone background

---
+++ Assets/Code/Scripts/Minimap/Waypoint Scriptable Object.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "new waypoint", menuName = "waypoint")]
+public class WaypointScriptableObject : ScriptableObject
+{
+    public Material emisionMaterial;
+    public Transform spawnPosition;
+    public Sprite logo;
+    
+    
+}
