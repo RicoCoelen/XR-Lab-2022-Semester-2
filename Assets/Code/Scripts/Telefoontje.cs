c298400fd639dd060f84ec55226e1a2bd6e1b8bc
DESKTOP-VRStudent <rico.coelen@gmail.com>
2023-01-26 11:48:55 +0100

    last changes

--- Assets/Code/Scripts/Telefoontje.cs
+++ Assets/Code/Scripts/Telefoontje.cs
@@ -9,6 +9,10 @@ public class Telefoontje : MonoBehaviour
     public AudioClip[] audioClips;
     private int audioIndex = 0;
 
+    private void Start()
+    {
+        source1 = GameObject.Find("LeftHand").GetComponent<AudioSource>();
+    }
     //if the player enters the dialogue area a series of dialogue fragments are played depending on the certain area.
     void OnTriggerStay(Collider collider)
     {
